f005165c78f2efdd22a0a563e307624762c63c96
moonshadow565 <moonshadow565@hotmail.com>
2022-02-17 14:00:22 +0100

    finish most important stuff

--- include/stl_msvc.hpp
+++ include/stl_msvc.hpp
@@ -4,45 +4,12 @@ namespace STL_MSVC_NS {
 #else
 namespace std {
 #endif
-    //! the default allocator
-    template <typename T>
-    struct allocator {};
+    struct __std_type_info_data {
+        const char* _UndecoratedName;
+        const char _DecoratedName[1];
+    };
 
-    //! std::hash specializations for fundamental, enumeration, and pointer types
-    template <typename T>
-    struct hash {};
-
-    //! function object implementing x == y
-    template <typename T>
-    struct equal_to {};
-
-    //! function object implementing x != y
-    template <typename T>
-    struct not_equal_to {};
-
-    //! function object implementing x > y
-    template <typename T>
-    struct greater {};
-
-    //! function object implementing x < y
-    template <typename T>
-    struct less {};
-
-    //! function object implementing x >= y
-    template <typename T>
-    struct greater_equal {};
-
-    //! function object implementing x <= y
-    template <typename T>
-    struct less_equal {};
-
-    //! Class Template which describes properties of a character type
-    template <typename T>
-    struct char_traits {};
-
-    //! default deleter for unique_ptr
-    template <typename T>
-    struct default_delete {};
+    using type_info = __std_type_info_data;
 
     //! conditonal::type is TrueT or FalseT depending on Condition
     template <bool Condition, typename TrueT, typename FalseT>
@@ -81,6 +48,8 @@ namespace std {
     //! static contiguous array
     template <typename T, size_t N>
     struct array {
+        using iterator = T*;
+
         T data[N];
     };
 
@@ -89,6 +58,12 @@ namespace std {
     struct bitset {
         using word_t = conditional_t<N <= sizeof(unsigned long) * 8, unsigned long, unsigned long long>;
         static constexpr inline size_t words_per_bit = sizeof(word_t) * 8;
+
+        struct reference {
+            word_t* word;
+            size_t offset;
+        };
+
         word_t words[N == 0 ? 1 : (N + words_per_bit - 1) / words_per_bit];
     };
 
@@ -113,7 +88,7 @@ namespace std {
     };
 
     //! stores and manipulates sequences of characters
-    template <typename C, typename T = char_traits<C>>
+    template <typename C>
     struct basic_string_view {
         using iterator = C*;
 
@@ -137,7 +112,7 @@ namespace std {
     using u32string_view = basic_string_view<char32_t>;
 
     //! stores and manipulates sequences of characters
-    template <typename C, typename T = char_traits<C>, typename A = allocator<C>>
+    template <typename C>
     struct basic_string {
         static_assert(sizeof(C) <= 8, "We need to be able to fit at least 2 chars in SSO buffer(16 bytes)!");
         union data_t {
@@ -167,31 +142,30 @@ namespace std {
     using u32string = basic_string<char32_t>;
 
     //! dynamic contiguous array
-    template <typename T, typename A = allocator<T>>
-    struct vector : A {
+    template <typename T>
+    struct vector {
         using iterator = T*;
 
-        // [[msvc::no_unique_address]] A alloc;
         T* beg;
         T* end;
         T* cap;
     };
 
     //! vector of bool specialization
-    template <typename A>
-    struct vector<bool, A> {
+    template <>
+    struct vector<bool> {
         struct iterator {
             unsigned int* ptr;
             size_t offset;
         };
 
-        vector<unsigned int, A> data;
+        vector<unsigned int> data;
         size_t size;
     };
 
     //! singly-linked list
-    template <typename T, typename A = allocator<T>>
-    struct forward_list : A {
+    template <typename T>
+    struct forward_list {
         struct node_t {
             node_t* next;
             T value;
@@ -200,13 +174,12 @@ namespace std {
             node_t* ptr;
         };
 
-        // [[msvc::no_unique_address]] A alloc;
         node_t* head;
     };
 
     //! doubly-linked list
-    template <typename T, typename A = allocator<T>>
-    struct list : A {
+    template <typename T>
+    struct list {
         struct node_t {
             node_t* prev;
             node_t* next;
@@ -216,84 +189,153 @@ namespace std {
             node_t* ptr;
         };
 
-        // [[msvc::no_unique_address]] A alloc;
         node_t* head;
         size_t size;
     };
 
+    template <typename Item>
+    struct tree {
+        enum color_t : char {
+            RED,
+            BLACK,
+        };
+
+        struct node_t {
+            // left subtree, or smallest element if head
+            node_t* left;
+            // parent, or root of tree if head
+            node_t* parent;
+            // right subtree, or largest element if head
+            node_t* right;
+            // RED or BLACK, BLACK if head
+            color_t color;
+            // true only if head (also nil) node; TRANSITION, should be bool
+            bool is_nil;
+            // the stored value, unused if head
+            Item item;
+        };
+
+        struct iterator {
+            node_t* ptr;
+        };
+
+        node_t* head;
+        size_t size;
+    };
+
+    template <typename Item>
+    struct hash {
+        using list = list<Item>;
+        using iterator = typename list::iterator;
+
+        // used for comparing
+        float traits_obj;
+        // list of elements
+        list list_elems;
+        // iterators for buckets. each bucket is 2 iterators denoting the locsed range of elements in
+        // the bucket.or both iterators set to end if bucket is empty
+        vector<iterator> vec_iters;
+        size_t mask;
+        size_t max_index;
+    };
+
     //! collection of unique keys, sorted by keys
-    template <typename K, typename L = less<K>, typename A = allocator<K>>
-    struct set;
+    template <typename K>
+    struct set : tree<K> {};
 
     //! collection of key-value pairs, sorted by keys, keys are unique
-    template <typename K, typename V, typename L = less<K>, typename A = allocator<pair<K, V>>>
-    struct map;
+    template <typename K, typename V>
+    struct map : tree<pair<K, V>> {};
 
-    //! collection of keys, sorted by keys
-    template <typename K, typename L = less<K>, typename A = allocator<K>>
-    struct multiset;
+    //! collection of keys, sorted by keys, duplicates allowed
+    template <typename K>
+    struct multiset : tree<K> {};
 
-    //! collection of key-value pairs, sorted by keys
-    template <typename K, typename V, typename L = less<K>, typename A = allocator<pair<K, V>>>
-    struct multimap;
+    //! collection of key-value pairs, sorted by keys, duplicates allowed
+    template <typename K, typename V>
+    struct multimap : tree<pair<K, V>> {};
 
     //! collection of unique keys, hashed by keys
-    template <typename K, typename H = hash<K>, typename E = equal_to<K>, typename A = allocator<K>>
-    struct unordered_set;
+    template <typename K>
+    struct unordered_set : hash<K> {};
 
     //! collection of key-value pairs, hashed by keys, keys are unique
-    template <typename K,
-              typename V,
-              typename H = hash<K>,
-              typename E = equal_to<K>,
-              typename A = allocator<pair<K, V>>>
-    struct unordered_map;
+    template <typename K, typename V>
+    struct unordered_map : hash<pair<K, V>> {};
 
-    //! collection of keys, hashed by keys
-    template <typename K, typename H = hash<K>, typename E = equal_to<K>, typename A = allocator<K>>
-    struct unordered_multiset;
+    //! collection of keys, hashed by keys, duplicates allowed
+    template <typename K>
+    struct unordered_multiset : hash<K> {};
 
-    //! collection of key-value pairs, hashed by keys
-    template <typename K,
-              typename V,
-              typename H = hash<K>,
-              typename E = equal_to<K>,
-              typename A = allocator<pair<K, V>>>
-    struct unordered_multimap;
+    //! collection of key-value pairs, hashed by keys, duplicates allowed
+    template <typename K, typename V>
+    struct unordered_multimap : hash<pair<K, V>> {};
 
     //! double-ended queue
-    template <typename T, typename A = allocator<T>>
-    struct deque;
+    template <typename T>
+    struct deque;  // TODO
 
     //! adapts a container to provide stack (LIFO data structure)
     template <typename T, typename C = deque<T>>
-    struct stack;
+    struct stack;  // TODO
 
     //! adapts a container to provide queue (FIFO data structure)
     template <typename T, typename C = deque<T>>
-    struct queue;
+    struct queue;  // TODO
 
-    //! adapts a container to provide priority queue
-    template <typename T, typename C = vector<T>, typename L = less<T>>
-    struct priority_queue;
+    //! default deleter for unique_ptr
+    template <typename T>
+    struct default_delete {};
 
     //! smart pointer with unique object ownership semantics
     template <typename T, typename D = default_delete<T>>
-    struct unique_ptr;
+    struct unique_ptr : D {
+        T* ptr;
+    };
+
+    struct ref_count_base {
+        virtual void destroy() noexcept = 0;
+        virtual void delete_this() noexcept = 0;
+        virtual ~ref_count_base() noexcept = 0;
+        virtual void* get_deleter(type_info const& tinfo) const noexcept = 0;
+        unsigned long uses;
+        unsigned long weaks;
+    };
 
     //! smart pointer with shared object ownership semantics
     template <typename T>
-    struct shared_ptr;
+    struct shared_ptr {
+        T* ptr;
+        ref_count_base* ref_count;
+    };
 
     //! weak reference to an object managed by std::shared_ptr
     template <typename T>
-    struct weak_ptr;
+    struct weak_ptr {
+        T* ptr;
+        ref_count_base* ref_count;
+    };
 
     //! allows an object to create a shared_ptr referring to itself
     template <typename T>
-    struct enable_shared_from_this;
+    struct enable_shared_from_this {
+        weak_ptr<T> this_weak_ptr;
+    };
 
     //! wraps callable object of any copy constructible type with specified function call signature
-    template <typename>
+    template <typename F>
     struct function;
+
+    template <typename RetT, typename... ArgT>
+    struct alignas(8) function<RetT(ArgT...)> {
+        struct impl_t {
+            virtual impl_t* copy(void* dst) const = 0;
+            virtual impl_t* move(void* dst) noexcept = 0;
+            virtual RetT call(ArgT&&...) = 0;
+            virtual const type_info& traget_type() const noexcept = 0;
+            virtual const void* get() const noexcept = 0;
+        };
+        void* storage[5 + 16 / sizeof(void*)];
+        impl_t* impl;
+    };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -48,43 +48,99 @@ struct fake_less {
 struct undefined;
 
 int main() {
-    TEST("array basic", array<char, 1>);
-    TEST("array of array", array<array<int, 3>, 5>);
+    TEST("", array<int, 3>);
+    TEST("", array<int, 3>::iterator);
 
-    TEST("bitset 0", bitset<0>);
-    TEST("bitset 7", bitset<7>);
-    TEST("bitset 9", bitset<9>);
-    TEST("bitset 32", bitset<32>);
-    TEST("bitset 65", bitset<65>);
+    TEST("", bitset<0>);
+    TEST("", bitset<0>::reference);
 
-    TEST("span", span<char>);
-    TEST("span", span<char, 1>);
-    TEST("span", span<char, (size_t)-1>);
+    TEST("", bitset<7>);
+    TEST("", bitset<9>);
+    TEST("", bitset<32>);
+    TEST("", bitset<65>);
 
-    TEST("pair of ints", pair<int, int>);
-    TEST("pair of int and char", pair<int, char>);
+    TEST("", span<char>);
+    TEST("", span<char>::iterator);
 
-    TEST("string_view", string_view);
-    TEST("u8string_view", u8string_view);
-    TEST("u16string_view", u16string_view);
-    TEST("u32string_view", u32string_view);
+    TEST("", span<char, 1>);
+    TEST("", span<char, 1>::iterator);
 
-    TEST("string", string);
-    TEST("u8string", u8string);
-    TEST("u16string", u16string);
-    TEST("u32string", u32string);
+    TEST("", pair<int, int>);
+    TEST("", pair<int, char>);
 
-    TEST("optional char", optional<char>);
-    TEST("optional int", optional<int>);
-    TEST("optional empty_t1", optional<empty_t1>);
-    TEST("optional of optional", optional<optional<char>>);
+    TEST("", string_view);
+    TEST("", string_view::iterator);
 
-    TEST("vector of char", vector<char>);
-    TEST("vector of bool", vector<bool>);
+    TEST("", wstring_view);
+    TEST("", wstring_view::iterator);
 
-    TEST("forward_list", forward_list<int>);
+    TEST("", u8string_view);
+    TEST("", u8string_view::iterator);
 
-    TEST("list", list<int>);
+    TEST("", u16string_view);
+    TEST("", u16string_view::iterator);
+
+    TEST("", u32string_view);
+    TEST("", u32string_view::iterator);
+
+    TEST("", string);
+    TEST("", string::iterator);
+
+    TEST("", wstring);
+    TEST("", wstring::iterator);
+
+    TEST("", u8string);
+    TEST("", u8string::iterator);
+
+    TEST("", u16string);
+    TEST("", u16string::iterator);
+
+    TEST("", u32string);
+    TEST("", u32string::iterator);
+
+    TEST("", optional<char>);
+    TEST("", optional<int>);
+    TEST("", optional<empty_t1>);
+    TEST("", optional<optional<char>>);
+
+    TEST("", vector<char>);
+    TEST("", vector<char>::iterator);
+
+    TEST("", vector<bool>);
+    TEST("", vector<bool>::iterator);
+
+    TEST("", forward_list<int>);
+    TEST("", forward_list<int>::iterator);
+
+    TEST("", set<int>);
+    TEST("", set<int>::iterator);
+
+    TEST("", map<int, int>);
+    TEST("", map<int, int>::iterator);
+
+    TEST("", multiset<int>);
+    TEST("", multiset<int>::iterator);
+
+    TEST("", multimap<int, int>);
+    TEST("", multimap<int, int>::iterator);
+
+    TEST("", unordered_set<int>);
+    TEST("", unordered_set<int>::iterator);
+
+    TEST("", unordered_map<int, int>);
+    TEST("", unordered_map<int, int>::iterator);
+
+    TEST("", unordered_multiset<int>);
+    TEST("", unordered_multiset<int>::iterator);
+
+    TEST("", unordered_multimap<int, int>);
+    TEST("", unordered_multimap<int, int>::iterator);
+
+    TEST("", unique_ptr<int>);
+    TEST("", shared_ptr<int>);
+    TEST("", weak_ptr<int>);
+    TEST("", enable_shared_from_this<int>);
+    TEST("", function<int(float, short)>);
 
     return 0;
 }
